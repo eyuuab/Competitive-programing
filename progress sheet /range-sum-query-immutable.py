3d89a238761960892cb275b834715cd2ca89f4c9
Eyobed Abreham <120777151+eyuuab@users.noreply.github.com>
2024-01-28 22:20:40 +0300

    Added solution for Range Sum Query - Immutable

---
+++ progress sheet /range-sum-query-immutable.py
@@ -0,0 +1,12 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.cum= [0]+list(accumulate(nums))
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.cum[right+1]-self.cum[left]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
